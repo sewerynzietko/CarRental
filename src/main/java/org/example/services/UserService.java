81818655c342b86c6f5dbc91ab2cef9c7d6144e5
sewerynzietko <sewezietko@gmail.com>
2026-04-14 18:08:03 +0200

    feat: implement logic in UserService

--- src/main/java/org/example/services/UserService.java
+++ src/main/java/org/example/services/UserService.java
@@ -1,20 +1,36 @@
 package org.example.services;
 
+import org.example.models.Rental;
 import org.example.models.User;
+import org.example.models.Vehicle;
 import org.example.repositories.UserRepository;
 
 import java.util.List;
 
 public class UserService {
+    UserRepository userRepository;
+    RentalService rentalService;
     public UserService(UserRepository userRepository, RentalService rentalService) {
+        this.userRepository = userRepository;
+        this.rentalService = rentalService;
     }
 
-    public void deleteUser(String s, String id) {
+    public void deleteUser(String userId, String loggedUserId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Nie znaleziono użytkownika."));
+
+        boolean rental = userRepository.equals("a");
+        if (rental) {
+            throw new IllegalArgumentException("Nie można usunć pojzdu, bo jest aktualnie wypożyczony.");
+        }
+        userRepository.deleteById(user.getId());
     }
 
     public List<User> findAllUsers() {
+        return userRepository.findAll();
     }
 
     public User findById(String id) {
+        return null;
     }
 }

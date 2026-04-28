646b5638f698c41b36260dcc9787c58eaf47ca7f
sewerynzietko <sewezietko@gmail.com>
2026-04-28 18:09:33 +0200

    refactor: update Main to use JDBC

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,37 @@
 package org.example;
 
 import org.example.repositories.*;
-import org.example.repositories.impl.*;
+import org.example.repositories.impl.jdbc.RentalJdbcRepository;
+import org.example.repositories.impl.jdbc.UserJdbcRepository;
+import org.example.repositories.impl.jdbc.VehicleCategoryConfigJdbcRepository;
+import org.example.repositories.impl.jdbc.VehicleJdbcRepository;
+import org.example.repositories.impl.json.RentalJsonRepository;
+import org.example.repositories.impl.json.UserJsonRepository;
+import org.example.repositories.impl.json.VehicleCategoryConfigJsonRepository;
+import org.example.repositories.impl.json.VehicleJsonRepository;
 import org.example.services.*;
 
 public class Main {
     public static void main(String[] args) {
-        VehicleRepository vehicleRepository = new VehicleJsonRepository();
-        UserRepository userRepository = new UserJsonRepository();
-        RentalRepository rentalRepository = new RentalJsonRepository();
-        VehicleCategoryConfigRepository categoryConfigRepository = new VehicleCategoryConfigJsonRepository();
+
+        VehicleRepository vehicleRepository;
+        UserRepository userRepository;
+        RentalRepository rentalRepository;
+        VehicleCategoryConfigRepository categoryConfigRepository;
+        if (args[0].equals("--storage=jdbc")) {
+            vehicleRepository = new VehicleJdbcRepository();
+            userRepository = new UserJdbcRepository();
+            rentalRepository = new RentalJdbcRepository();
+            categoryConfigRepository = new VehicleCategoryConfigJdbcRepository();
+            //System.out.println(vehicleRepository.findAll());
+        } else {
+            vehicleRepository = new VehicleJsonRepository();
+            userRepository = new UserJsonRepository();
+            rentalRepository = new RentalJsonRepository();
+            categoryConfigRepository = new VehicleCategoryConfigJsonRepository();
+        }
+
+
 
         AuthService authService = new AuthService(userRepository);
         VehicleCategoryConfigService categoryConfigService = new VehicleCategoryConfigService(categoryConfigRepository);

52c89646d83acc7550d8555dfb72bf19c5a3054a
sewerynzietko <sewezietko@gmail.com>
2026-05-05 17:43:03 +0200

    feat: implement hibernate repository classes

--- src/main/java/org/example/repositories/impl/hibernate/RentalHibernateRepository.java
+++ src/main/java/org/example/repositories/impl/hibernate/RentalHibernateRepository.java
@@ -2,33 +2,54 @@ package org.example.repositories.impl.hibernate;
 
 import org.example.models.Rental;
 import org.example.repositories.RentalRepository;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public class RentalHibernateRepository implements RentalRepository {
+
+    private Session session;
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+
     @Override
     public List<Rental> findAll () {
-        return null;
+        return session.createQuery("FROM Rental", Rental.class).list();
     }
 
     @Override
     public List<Rental> findById ( String id ) {
-        return null;
+        return Optional.ofNullable(session.get(Rental.class, id)).stream().toList();
     }
 
     @Override
     public Rental save ( Rental rental ) {
-        return null;
+        return session.merge(rental);
     }
 
     @Override
     public void deleteById ( String id ) {
+        Rental rental = session.get(Rental.class, id);
 
+        if (rental != null) {
+            session.remove(rental);
+        }
     }
 
     @Override
     public Optional<Rental> findByVehicleIdAndReturnDateIsNull ( String vehicleId ) {
-        return Optional.empty();
+        Query<Rental> query = session.createQuery("""
+                FROM Rental r
+                WHERE r.vehicle.id = :vehicleId
+                AND r.returnDateTime IS NULL
+                """, Rental.class);
+
+        query.setParameter("vehicleId", vehicleId);
+
+        return query.uniqueResultOptional();
     }
 }
--- src/main/java/org/example/repositories/impl/hibernate/VehicleHibernateRepository.java
+++ src/main/java/org/example/repositories/impl/hibernate/VehicleHibernateRepository.java
@@ -1,31 +1,40 @@
 package org.example.repositories.impl.hibernate;
 
+import org.example.models.Rental;
 import org.example.models.Vehicle;
 import org.example.repositories.VehicleRepository;
+import org.hibernate.Session;
 
 import java.util.List;
 import java.util.Optional;
 
 public class VehicleHibernateRepository implements VehicleRepository {
+    private Session session;
 
-
+    public void setSession(Session session) {
+        this.session = session;
+    }
     @Override
     public List<Vehicle> findAll () {
-        return null;
+        return session.createQuery("FROM Vehicle", Vehicle.class).list();
     }
 
     @Override
     public Optional<Vehicle> findById ( String id ) {
-        return Optional.empty();
+        return Optional.ofNullable(session.get(Vehicle.class, id));
     }
 
     @Override
     public Vehicle save ( Vehicle vehicle ) {
-        return null;
+        return session.merge(vehicle);
     }
 
     @Override
     public void deleteById ( String id ) {
+        Vehicle vehicle = session.get(Vehicle.class, id);
 
+        if (vehicle != null) {
+            session.remove(vehicle);
+        }
     }
 }

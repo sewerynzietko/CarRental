a228f5add7db440ac1af6bca14eaf587001a6ea7
sewerynzietko <sewezietko@gmail.com>
2026-06-02 18:01:38 +0200

    feat: modify rent response with BodyRequest and logged in User

--- src/main/java/org/example/web/RentalController.java
+++ src/main/java/org/example/web/RentalController.java
@@ -1,8 +1,14 @@
 package org.example.web;
 
+import org.example.dto.RentalRequest;
 import org.example.models.Rental;
+import org.example.models.User;
 import org.example.services.RentalServiceInterface;
 import org.example.services.UserServiceInterface;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,8 +17,10 @@ import java.util.List;
 @RequestMapping("/api/rentals")
 public class RentalController {
     private final RentalServiceInterface rentalService;
-    public RentalController ( RentalServiceInterface rentalService ) {
+    private final UserServiceInterface userService;
+    public RentalController ( RentalServiceInterface rentalService, UserServiceInterface userService ) {
         this.rentalService = rentalService;
+        this.userService = userService;
     }
     @GetMapping
     public List<Rental> list(){
@@ -22,9 +30,18 @@ public class RentalController {
     public List<Rental> userRentals(@PathVariable String userId){
         return rentalService.findUserRentals(userId);
     }
-    @PostMapping("/users/{userId}/rent/{vehicleId}")
-    public Rental rent(@PathVariable String userId, @PathVariable String vehicleId){
-        return rentalService.rentVehicle(userId, vehicleId);
+    @PostMapping("/rent")
+    public ResponseEntity<Rental> rent(
+            @RequestBody RentalRequest rentalRequest,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        String login = userDetails.getUsername();
+        User user = userService.findByLogin(login);
+        Rental rental = rentalService.rentVehicle(
+                user.getId(),
+                rentalRequest.VehicleId()
+        );
+        return ResponseEntity.status(HttpStatus.CREATED).body(rental);
     }
     @PostMapping("/users/{userId}/return")
     public Rental returnVehicle(@PathVariable String userId){

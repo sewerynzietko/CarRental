5617e3403d5881f048072a32326f09ca3b9aa3ec
sewerynzietko <sewezietko@gmail.com>
2026-06-09 17:38:07 +0200

    feat: add getUserRole method for token generation

--- src/main/java/org/example/security/JwtUtil.java
+++ src/main/java/org/example/security/JwtUtil.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -60,6 +61,13 @@ public class JwtUtil {
     private Key getSigningKey() {
         byte[] keyBytes = Decoders.BASE64URL.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private String getUserRole(UserDetails userDetails){
+        return userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .findFirst()
+                .orElse("USER");
     }
 
 }

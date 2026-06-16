59445c16b35058b0b032918163be336107abd09a
sewerynzietko <sewezietko@gmail.com>
2026-06-16 17:23:48 +0200

    feat: refactor role for many roles

--- src/main/java/org/example/models/Role.java
+++ src/main/java/org/example/models/Role.java
@@ -1,5 +1,27 @@
 package org.example.models;
 
-public enum Role {
-    USER, ADMIN;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
+@ToString(exclude = "users")
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @Column(nullable = false, unique = true)
+    private String id;
+    @Column(nullable = false, unique = true)
+    private String name; // ROLE_USER, ROLE_ADMIN
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore
+    private Set<User> users;
 }

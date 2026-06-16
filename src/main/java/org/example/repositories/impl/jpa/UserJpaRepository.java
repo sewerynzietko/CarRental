cca35a1c29e3023e550dadf99066943f7e60fa5f
sewerynzietko <sewezietko@gmail.com>
2026-06-16 17:39:38 +0200

    feat: add EntityGraph for many roles

--- src/main/java/org/example/repositories/impl/jpa/UserJpaRepository.java
+++ src/main/java/org/example/repositories/impl/jpa/UserJpaRepository.java
@@ -3,6 +3,7 @@ package org.example.repositories.impl.jpa;
 import org.example.models.User;
 import org.example.repositories.UserRepository;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,6 @@ import java.util.Optional;
 @Repository
 @Profile("jpa")
 public interface UserJpaRepository extends JpaRepository<User, String>, UserRepository {
+    @EntityGraph(attributePaths = "roles")
     Optional<User> findByLogin(String login);
 }

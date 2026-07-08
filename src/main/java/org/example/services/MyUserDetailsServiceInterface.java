9604b3bb58d3b9e339f7710ce785dbb877f4072a
sewerynzietko <sewezietko@gmail.com>
2026-07-08 20:24:34 +0200

    chore: doubled files deletion

--- src/main/java/org/example/services/MyUserDetailsServiceInterface.java
+++
@@ -1,7 +0,0 @@
-package org.example.services;
-
-import org.springframework.security.core.userdetails.UserDetails;
-
-public interface MyUserDetailsServiceInterface {
-    public UserDetails loadUserByUsername(String username);
-}

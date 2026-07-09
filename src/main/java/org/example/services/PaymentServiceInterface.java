702703fb0a55be9b9583425a437b12fcd25d1af6
sewerynzietko <sewezietko@gmail.com>
2026-07-09 16:34:55 +0200

    feat: bussines logic for payment

--- src/main/java/org/example/services/PaymentServiceInterface.java
+++ src/main/java/org/example/services/PaymentServiceInterface.java
@@ -1,4 +1,7 @@
 package org.example.services;
 
 public interface PaymentServiceInterface {
+    String createCheckoutSession(String rentalId);
+
+    void handleWebhook(String payload, String signature);
 }

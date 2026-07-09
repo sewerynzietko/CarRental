d3ee862ba641ae811217039f36634c1c0e804576
sewerynzietko <sewezietko@gmail.com>
2026-07-09 19:29:17 +0200

    feat: enable scheduling for vehicle location

--- src/main/java/org/example/CarRentApplication.java
+++ src/main/java/org/example/CarRentApplication.java
@@ -2,7 +2,10 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 @SpringBootApplication
+@EnableScheduling
 public class CarRentApplication {
     public static void main(String[] args) {
         SpringApplication.run(CarRentApplication.class, args);

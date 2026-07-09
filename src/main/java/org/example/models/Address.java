827dc7044895abb7b423f285cb1aea19e500395a
sewerynzietko <sewezietko@gmail.com>
2026-07-09 17:11:56 +0200

    feat: model class for address

--- src/main/java/org/example/models/Address.java
+++ src/main/java/org/example/models/Address.java
@@ -1,4 +1,18 @@
 package org.example.models;
 
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Address {
+    private String city;
+    private String street;
+    private String postalCode;
 }

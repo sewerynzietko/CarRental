ba2e550fc4ec51d494f568cbc87394d542decf27
sewerynzietko <sewezietko@gmail.com>
2026-07-09 17:11:26 +0200

    feat: restructure address fields in RegisterRequest

--- src/main/java/org/example/dto/RegisterRequest.java
+++ src/main/java/org/example/dto/RegisterRequest.java
@@ -2,6 +2,9 @@ package org.example.dto;
 
 public record RegisterRequest(
         String login,
-        String password
-) { }
+        String password,
+        String city,
+        String street,
+        String postalCode
+) {}
 
